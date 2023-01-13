14d5629a3dc5505d4c28137273cd24a7817c0b6a
thehuseyinoglu <thehuseyinoglu@gmail.com>
2023-01-14 01:28:01 +0300

    Create Components

---
+++ src/components/Summary.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-green-300">{{completed}} adet tamamlanmis taskiniz bulunmaktadir</div>
+    <div class="text-red-400">{{unCompleted}} adet tamamlanmamis taskiniz bulunmaktadir</div>
+</template>
+<script>
+import { computed } from "vue"
+export default {
+    props: {
+        todoList: Array
+    },
+    setup(props) {
+        const completed = computed(() => props.todoList.filter((i) => i.completed).length)
+        const unCompleted = computed(() => props.todoList.filter((i) => !i.completed).length)
+
+        return {
+            completed, unCompleted
+        }
+    }
+}
+</script>
